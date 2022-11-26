fa0ef333153fac69e785a4ac72562905a4e8723e
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-11-26 18:04:32 +0530

    Find max number in three float number

---
+++ FindMaxNumberUsingGenerics/FindMaxNumberUsingGenerics/Floats_FindMaxNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNumberUsingGenerics
+{
+    internal class Floats_FindMaxNum
+    {
+        public static float Find_Max_num(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0
+              || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0
+              || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0
+              || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0
+              || num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0
+              || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0
+              || num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return 0;
+        }
+    }
+}
--- FindMaxNumberUsingGenerics/FindMaxNumberUsingGenerics/Program.cs
+++ FindMaxNumberUsingGenerics/FindMaxNumberUsingGenerics/Program.cs
@@ -2,3 +2,5 @@
 using System.Net.Security;
 
 Console.WriteLine("Hello world");
+
+Console.WriteLine(Floats_FindMaxNum.Find_Max_num(10.5f, 20.5f, 30.5f));
