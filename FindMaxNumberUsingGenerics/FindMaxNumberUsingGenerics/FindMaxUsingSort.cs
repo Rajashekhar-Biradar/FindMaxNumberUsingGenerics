72766d95dfb849b6073e77f69a87b11fbf11ec04
Rajashekhar-Biradar <rajashekharbiradar41@gmail.com>
2022-11-28 13:06:12 +0530

    Find the maximum number using sort method

---
+++ FindMaxNumberUsingGenerics/FindMaxNumberUsingGenerics/FindMaxUsingSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxNumberUsingGenerics
+{
+    public class FindMaxUsingSort<T> where  T : IComparable<T>
+    {
+        public T[] value;
+
+        public FindMaxUsingSort(T[] value)
+        {
+            this.value = value;
+        }
+
+
+        public void sort()
+        {
+           Array.Sort(value);
+            var max = value[^1];
+            Console.WriteLine(max);
+
+        }
+    }
+}
